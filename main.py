fceb67b85358fdfed11fe3327a0382eb376f4969
krllmm <137904300+krllmm@users.noreply.github.com>
2024-07-14 21:14:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import io
+import streamlit as st
+from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0 # type: ignore
+from tensorflow.keras.preprocessing import image # type: ignore
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions # type: ignore
+
+
+@st.cache_data()
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+model = load_model()
+
+with st.sidebar:
+    st.title("Информация о команде")
+    st.text("Главный разработчик: Я")
+    st.text("Обучатель сети: не Я")
+
+st.title('Новая улучшенная классификации изображений в облаке Streamlit')
+
+img = load_image()
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+
+    st.divider()
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
