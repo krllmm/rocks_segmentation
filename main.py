032d13e0b8afdb9d4e26559b2359a33de3225895
krllmm <137904300+krllmm@users.noreply.github.com>
2024-07-16 11:54:49 +0500

    added newral network

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import io
 import streamlit as st
 from PIL import Image
 import numpy as np
-from tensorflow.keras.applications import EfficientNetB0 
-from tensorflow.keras.preprocessing import image 
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions 
-
+from PIL import ImageDraw
+from ultralytics import YOLO
+from io import BytesIO
 
 st.set_page_config(
-    page_title="Rock Segmantation",
+    page_title="Rock Segmentation",
     page_icon="🗿",
     layout="wide",
     initial_sidebar_state="collapsed"
@@ -16,16 +15,7 @@ st.set_page_config(
 
 @st.cache_data()
 def load_model():
-    return EfficientNetB0(weights='imagenet')
-
-
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
-
+    return YOLO("best.pt")  
 
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для сегментации')
@@ -35,14 +25,6 @@ def load_image():
         return Image.open(io.BytesIO(image_data))
     else:
         return None
-
-
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], cl[2])
-    
-
 
 model = load_model()
 
@@ -56,14 +38,31 @@ st.title("Сегментация изображения горных пород"
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
+    results = model(img)
 
     st.divider()
 
     st.success('Your edited image was processes! 🎉')
     st.write('**Результаты распознавания:**')
-    print_predictions(preds)
-    #st.download_button("Сохранить")
 
+    result = results[0]
+    masks = result.masks
+    for mask in masks:
+        polygon = mask.xy[0]
+        draw = ImageDraw.Draw(img)
+        draw.polygon(polygon,outline=(0,255,0), width=5)
 
+    segmented = draw._image
+
+    st.image(segmented)
+    
+    buf = BytesIO()
+    img.save(buf, format="JPEG")
+    byte_im = buf.getvalue()
+
+    st.download_button(
+        label="Download Image",
+        data=byte_im,
+        file_name="segmented_rocks.png",
+        mime="image/jpeg",
+    )
